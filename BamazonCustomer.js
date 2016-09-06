ae08a038a9968b332d9a176e720eddc87eebc786
John Grant Bunner <scribe@exposed2tce.com>
2016-09-05 22:48:54 -0400

    Bamazon

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root", //Your username
     password: passWord, //Your password
-    database: "topsongsdb"
+    database: "bamazondb"
 })
 
 connection.connect(function(err) {
@@ -17,127 +17,61 @@ connection.connect(function(err) {
 })
 
 var runSearch = function() {
+	
+	connection.query("SELECT * FROM products", function(err, res){
+	if(err) throw err;
+	
+	console.log("\nItem ID: Product Name : Department Name : $Price : StockQuantity" );
+	
+	for (i in res){
+		console.log(res[i].itemID + ": " + res[i].productName + " : " + res[i].departmentName + " : $" + res[i].price + " : " + res[i].stockQuantity );
+	}
+	selectItem();
+	});
+}
+var	selectItem = function(){
     inquirer.prompt({
         name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["Find songs by artist", "Find all artists who appear more than once", "Find data within a specific range", "Search for a specific song"]
+        type: "input",
+        message: "\nEnter the ItemID of what would you like to buy. \nEnter Q to quit.",
     }).then(function(answer) {
-        switch(answer.action) {
-            case 'Find songs by artist':
-                artistSearch();
-            break;
-            
-            case 'Find all artists who appear more than once':
-                multiSearch();
-            break;
-            
-            case 'Find data within a specific range':
-                rangeSearch();
-            break;
-            
-            case 'Search for a specific song':
-                songSearch();
-            break;
-        }
-    })
+				if (answer.action.toUpperCase() == "Q"){
+					connection.end(function(err){
+					console.log("\nHave a nice day!");
+					});
+					}
+				else if (answer.action > 0){
+                buyItem(answer.action);
+	}
+	else {console.log("Invalid input. \nPlease retry.");
+	runSearch();}
+	})
 };
 
-var artistSearch = function() {
+var buyItem = function(itemID) {
 	    inquirer.prompt({
-        name: "artistName",
+        name: "itemQuantity",
         type: "input",
-        message: "What artist would like to look for?",
+        message: "How Many do you want?",
     }).then(function(answer) {
-	connection.query("SELECT * FROM top5000 WHERE ?",{ artist: answer.artistName}, function(err, res){
-	if(err) throw err;
-	for(i in res){
-	console.log(res[i].song);
-	}	
+		
+		connection.query("SELECT * FROM products WHERE ?",{ itemID: itemID}, function(err, res){
+		if(err) throw err;
+		
+		isEnough = res[0].stockQuantity - answer.itemQuantity;
+		console.log(isEnough);
+		if (isEnough >= 0){
+			connection.query("UPDATE products SET ? WHERE ?", [{ stockQuantity: isEnough},
+			{itemID: itemID}], function(err, res){});
+			console.log("Transaction processed.\nWould you like somthing else?");
+			runSearch();
+			
+		}
+		else {console.log("Sorry we don't have enough to sell you that many. \nWould you like something else?");
+		runSearch();
+		}	
+	
+		});
+		
 	});
-
-connection.end(function(err){
-	console.log("\nprocess ended")
-	})});
-
-}
-
-var songSearch = function() {
-	    inquirer.prompt({
-        name: "songName",
-        type: "input",
-        message: "What song would like to look for?",
-    }).then(function(answer) {
-	connection.query("SELECT * FROM top5000 WHERE ?",{ song: answer.songName}, function(err, res){
-	if(err) throw err;
-	console.log(res);	
-	
-});
-
-connection.end(function(err){
-	console.log("\nprocess ended")
-})});
-
-}
-
-var multiSearch = function() {
-	connection.query("SELECT artist FROM top5000", function(err, res){
-	if(err) throw err;
-	var list = res;
-	var output = [];
-	for (i in list){
-		for (e in list){
-			if(i != e){
-				if (list[i].artist == list[e].artist){
-					var inList = false;
-					for (q in output){
-						if (list[i].artist == output[q]){
-							inList = true;
-							break;
-						}
-					}
-					if (inList == false){
-					output.push(list[i].artist);
-					}
-				}
-			}
-		}
-	}
-	console.log(output)
-	console.log(output.length);
-});
-
-connection.end(function(err){
-	console.log("\nprocess ended")
-});
-
-}
-
-var rangeSearch = function() {
-	    inquirer.prompt([{
-        name: "startYear",
-        type: "input",
-        message: "What year would like to start at?",
-    },{
-        name: "endYear",
-        type: "input",
-        message: "What year would like to end at?",
-    }]).then(function(answer) {
-	connection.query("SELECT * FROM top5000", function(err, res){
-	if(err) throw err;
-	var list = res;
-	var years = [];
-	for (i in list){
-		if (list[i].year >= answer.startYear && list[i].year <= answer.endYear){
-			years.push(list[i]);
-		}
-	}
-	console.log(years);
-	
-});
-
-connection.end(function(err){
-	console.log("\nprocess ended")
-	})});
-
-}
+};
